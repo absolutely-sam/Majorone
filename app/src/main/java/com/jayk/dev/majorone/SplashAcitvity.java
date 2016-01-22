c68e05aa41f4ab343afea989fb1372b78f2082d3
jayakrishnan nair <kjayknair@gmail.com>
2016-01-22 15:17:57 +0530

    all files included

--- app/src/main/java/com/jayk/dev/majorone/SplashAcitvity.java
+++ app/src/main/java/com/jayk/dev/majorone/SplashAcitvity.java
@@ -9,10 +9,10 @@ package com.jayk.dev.majorone;
 
 //Note:Don't include the code in repository without testing
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class SplashAcitvity extends AppCompatActivity {
+public class SplashAcitvity extends Activity {
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
